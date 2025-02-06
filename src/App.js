72720ceb212a6735f6ecfe81a0d5d445f60e0bf7
Ramya19manda <2211Cs010625@mallareddyuniversity.ac.in>
2025-02-06 12:59:28 +0530

    pushing the files into repo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,78 @@
-import React from 'react';
-import Navbar from './components/Navbar1';
-import Main from './components/Main';
-import Sidebar1 from './components/Sidebar1';
-import Sidebar2 from './components/Sidebar2';
-import Footer from './components/Footer';
-import './components/global.css';
+// import React from 'react';
+// import Navbar from './components/Navbar1';
+// import Main from './components/Main';
+// import Sidebar1 from './components/Sidebar1';
+// import Sidebar2 from './components/Sidebar2';
+// import Footer from './components/Footer';
+// import './components/global.css';
 
+// const App = () => {
+//   return (
+//     <div className="app">
+//       <div className="navbar">Navbar1</div>
+//       <div className="main">Main</div>
+//       <div className="sidebar">
+//         <div className="sidebar1">Sidebar1</div>
+//         <div className="sidebar2">Sidebar2</div>
+//       </div>
+//       <div className="footer">Footer</div>
+//     </div>
+//   );
+// };
+// export default App;
+
+// import React, {Component} from 'react'
+// // import CBCPropEx from './propsex/CBCPropEx'
+// import FBCPropEx from './propsex/FBCPropEx'
+
+// export default class App extends Component{
+//   render(){
+//     return(
+//     <div>
+//       {/* <CBCPropEx
+//       username="ramya"
+//       age={19}
+//       hobbies={["playing","dancing"]}
+//       address={{city:"jangaon",area:"Rameshwaram"}}
+//       sendFun={function(){alert("hi i am ramya")}}
+//       /> */}
+//       <FBCPropEx
+//       username="abhinay"
+//       isMarried={true}
+//       hobbies={["chatting","Reading","eating"]}
+//       />
+//     </div>
+//     )
+//   }
+// }
+
+// import React from "react";
+
+// import CBCStateEX from "./stateexample/CBCStateEx";
+// const App = () => {
+//   return (
+//     <div>
+//       <App/>
+//       <CBCStateEX />
+    
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+
+
+
+import React from "react";
+import FBCStateEx from "./stateexample/FBCStateEx";
 const App = () => {
   return (
-    <div className="app">
-      <div className="navbar">Navbar1</div>
-      <div className="main">Main</div>
-      <div className="sidebar">
-        <div className="sidebar1">Sidebar1</div>
-        <div className="sidebar2">Sidebar2</div>
-      </div>
-      <div className="footer">Footer</div>
+    <div>App <FBCStateEx/>      
     </div>
   );
 };
+
 export default App;
+
---
+++ src/propsex/FBCPropEx.jsx
@@ -0,0 +1,20 @@
+// import React from 'react'
+// import { connect } from 'react-redux'
+
+// export const FBCPropEx = (props) => {
+//     console.log(props);
+//     if(props.isMarried===true){
+//       return (
+//         <div>
+//            <h1>{props.username} is a married person</h1>
+//            {
+//                 props.hobbies.map(hobby=>{
+//                   return <li>{hobby}</li>
+//                 })
+//             }
+//         </div>
+//     )
+//   }else{
+//     return <h1> Not married</h1>
+//   }
+// }
